117f36f467bbb336cf76b2fbaf6e4922d1f2f2f6
kelvintyh <kelvinthammZZLeo@gmail.com>
2019-10-24 07:54:41 +0800

    deleted unused comments

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -24,32 +24,9 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{ rollDice() }
 
-
-       // val countUpButton: Button = findViewById(R.id.countUp_button)
-       // countUpButton.setOnClickListener{ countUp()}
-
-
     }
 
     private fun rollDice(){
-        //Toast.makeText(this, "button clicked",
-        //Toast.LENGTH_SHORT ).show()
-
-        /*  Three Dice
-        val randomInt_1 = Random().nextInt(6) + 1
-        val randomInt_2 = Random().nextInt(6) + 1
-        val randomInt_3 = Random().nextInt(6) + 1
-
-        val resultText_1: TextView = findViewById(R.id.result_text_1)
-        val resultText_2: TextView = findViewById(R.id.result_text_2)
-        val resultText_3: TextView = findViewById(R.id.result_text_3)
-        resultText_1.text = randomInt_1.toString()
-        resultText_2.text = randomInt_2.toString()
-        resultText_3.text = randomInt_3.toString()
-         */
-
-        //val result : TextView = findViewById(R.id.result_text_4)
-        //result.text = randomInt.toString()
 
         val diceImage1 : ImageView = findViewById(R.id.dice_image_1)
         val diceImage2 : ImageView = findViewById(R.id.dice_image_2)
@@ -72,21 +49,4 @@ class MainActivity : AppCompatActivity() {
         return drawableResource
     }
 
-    /*
-    private fun countUp(){
-
-
-        val resultText_1: TextView = findViewById(R.id.result_text_4)
-        val resultText_2: TextView = findViewById(R.id.result_text_5)
-
-        if(!resultText_1.text.equals("6")) {
-            try {
-                val x = resultText_1.text.toString().toInt() + 1;
-                resultText_2.text = x.toString()
-
-            } catch (nfe: NumberFormatException) {
-                // not a valid int
-            }
-        }
-    }*/
     }
